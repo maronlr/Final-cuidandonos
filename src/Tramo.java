b332ec3318b8c93752e0bbb14f20c7ebd26b49c6
Maron Leonel Rabahia <mrabahia@frba.utn.edu.ar>
2024-05-01 13:22:13 -0300

    Correcciones

--- src/Tramo.java
+++ src/Tramo.java
@@ -1,7 +1,15 @@
+package src;
+
 import java.time.Duration;
 
 public class Tramo {
+    private static final int factorMinutosPorMetro = 2; // Factor de cantidad de tiempo por distancia
     public Duration demora(){
-        return Duration.ofMinutes(0); //TODO
+        return Duration.ofMinutes((long) factorMinutosPorMetro * distancia());
+    }
+
+    public int distancia(){
+        int llamadoAPI = 10; //TODO Donde se llama la Distance Matrix API
+        return llamadoAPI;
     }
 }
